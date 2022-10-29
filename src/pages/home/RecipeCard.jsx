194438ed56377afff5d7aefac45789f82c16c5fb
Schwayzer <furkann.onay@gmail.com>
2022-10-29 13:48:53 +0300

    added useNavigate

--- src/pages/home/RecipeCard.jsx
+++ src/pages/home/RecipeCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { RecipeDiv, RecipeCards, Button } from "./Home.styled";
 
 const RecipeCard = ({ recipes }) => {
+  const navigate = useNavigate();
   const handleClick = (e) => {
     e.preventDefault();
     console.log(e.target.id);
+    navigate("/details");
   };
   return (
     <RecipeDiv>
       {recipes.map((recipe) => {
         const { uri, images, label } = recipe.recipe;
         const recipeID = uri.split("recipe_")[1];
+        // console.log(recipeID);
         return (
           <RecipeCards>
             <h3>{label}</h3>
